8336e5a1495a289a2746c0c821810746f17d3c40
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-18 19:36:45 +0300

    adding first version only

---
+++ Template/wrapper/src/index.js
@@ -0,0 +1,19 @@
+/* eslint import/extensions: 0 */
+
+
+import Logo from './logo/Logo.jsx';
+import PreviewText from './previewText/PreviewText.jsx';
+import Sponsor from './sponsor/Sponsor.jsx';
+
+import HOC from './HOC/HOC.jsx';
+import BodyPrototype from './bodyPrototype/BodyPrototype.js';
+
+// TODO: find a better way to keep it at some place
+import headStyles from './head-styles/index.js';
+
+export {
+    Logo, PreviewText, Sponsor,
+
+    HOC,
+    BodyPrototype
+};
