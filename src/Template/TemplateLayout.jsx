9c1f5636e6e633d96f092b849f4473e183b29b56
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-29 16:13:25 +0300

    Update TemplateLayout.jsx

--- src/Template/TemplateLayout.jsx
+++ src/Template/TemplateLayout.jsx
@@ -1,2 +1,4 @@
 // only simple components should be here.
 // like a HOC
+// also we can pass some default prop things, like required and stuff
+// it will make this template necessary and orbituary to use
